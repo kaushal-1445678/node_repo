afbaa05c7c9da9936f928cdb6009cb0dc3b6e6f4
kaushal-1445678 <kaushal730223@gmail.com>
2026-03-16 00:30:14 +0000

    update day 22

--- day 22/backend-app/server.js
+++ day 22/backend-app/server.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const app=express();
 
+app.use(express.json());
+
 app.get('/',function(req,res){
     res.send('Hello from the server\'s browser');
 
@@ -8,20 +10,62 @@ app.get('/',function(req,res){
 app.get('/about',(req,res)=>{
     res.send('This is the about page');
 });
+//REST APIs (GET, POST, PUT, DELETE)
+
+//GET API:-
+const users=[ 
+    {
+        id:1,
+        name:'kaushal tyagi'
+
+    },
+    {
+        id:2,
+        name:'jon snow'
+    }
+];
 
 app.get('/users',(req,res)=>{
-    const users=[ 
-        {
-            id:1,
-            name:'kaushal tyagi'
-
-        },
-        {
-            id:2,
-            name:'jon snow'
-        }
-    ]
     res.json(users);
+});
+
+// POST API:-
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+  users.push(newUser);
+  res.json({
+    message: "User added",
+    users: users
+  });
+});
+
+// PUT API:-
+
+app.put('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const updatedName = req.body.name;
+
+  users = users.map(user => {
+    if(user.id === id){
+      user.name = updatedName;
+    }
+    return user;
+  });
+
+  res.json(users);
+});
+
+// DELETE API:-
+app.delete('/users/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  users = users.filter(user => user.id !== id);
+  // users=users.filter(function(user) { return user.id !== id; });
+
+  res.json({
+    message:"User deleted",
+    users:users
+  });
 });
 app.listen(3000,function(){
     console.log('Server is running on port 3000');
